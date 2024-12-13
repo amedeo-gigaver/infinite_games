afeab4162365f88b5620d7ea5fb3e89e96bdc90d
MadMedian <93577855+MadMedian@users.noreply.github.com>
2024-12-13 18:28:39 +0100

    INF-141 Bump version to 1.3.3 (#56)

--- infinite_games/__init__.py
+++ infinite_games/__init__.py
@@ -15,7 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__version__ = "1.3.2"
+__version__ = "1.3.3"
 
 version_split = __version__.split(".")
 __spec_version__ = (
